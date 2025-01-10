1470961f8d4e2458609ab1fb428520b5d91bee89
tariq099 <xeltoroxabdalla@gmail.com>
2025-01-10 08:58:48 +0200

    initial commit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-  <h2 class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Welcome To Workopia</h2>
+  <h2 class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3"></h2>
   <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
     @forelse($jobs as $job)
     <x-job-card :job="$job" />
